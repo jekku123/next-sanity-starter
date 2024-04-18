9b8ae69abc209c6b9719bbad72c0eada0885f0e8
Jesse Manninen <jesse.manninen@gmail.com>
2024-04-18 14:02:20 +0300

    added Cypress. + layout and darkkmode tests

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,22 @@
+describe("template spec", () => {
+  it("Visits the frontpage and checks that the main components are in place", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("header").should("be.visible").should("exist");
+    cy.get("main").should("be.visible").should("exist");
+    cy.get("footer").should("be.visible").should("exist");
+  });
+});
+
+describe("dark mode and light mode", () => {
+  it("Visits the frontpage and checks that the light and dark modes work", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.get('[data-test-id="mode-toggle"]').click();
+    cy.get('[data-test-id="lightmode-button"]').click();
+    cy.get("html").should("have.class", "light");
+
+    cy.get('[data-test-id="mode-toggle"]').click();
+    cy.get('[data-test-id="darkmode-button"]').click();
+    cy.get("html").should("have.class", "dark");
+  });
+});
--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -17,17 +17,23 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" data-test-id="mode-toggle">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("light")}
+          data-test-id="lightmode-button"
+        >
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("dark")}
+          data-test-id="darkmode-button"
+        >
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
