56fe6a1b09d79197441a12e8ed155ee39e01dc1a
Jesse Manninen <jesse.manninen@gmail.com>
2024-04-18 12:14:01 +0300

    remove classNames from blockcontent contents

--- src/components/block-content.tsx
+++ src/components/block-content.tsx
@@ -28,27 +28,15 @@ export default async function BlockContent({
 }: BlockContentProps) {
   const components = {
     block: {
-      h1: ({ children }: any) => (
-        <TypographyH1 className={className}>{children}</TypographyH1>
-      ),
-      h2: ({ children }: any) => (
-        <TypographyH2 className={className}>{children}</TypographyH2>
-      ),
-      h3: ({ children }: any) => (
-        <TypographyH3 className={className}>{children}</TypographyH3>
-      ),
-      h4: ({ children }: any) => (
-        <TypographyH4 className={className}>{children}</TypographyH4>
-      ),
+      h1: ({ children }: any) => <TypographyH1>{children}</TypographyH1>,
+      h2: ({ children }: any) => <TypographyH2>{children}</TypographyH2>,
+      h3: ({ children }: any) => <TypographyH3>{children}</TypographyH3>,
+      h4: ({ children }: any) => <TypographyH4>{children}</TypographyH4>,
       normal: ({ children }: any) => (
-        <TypographyParagraph className={className}>
-          {children}
-        </TypographyParagraph>
+        <TypographyParagraph>{children}</TypographyParagraph>
       ),
       blockquote: ({ children }: any) => (
-        <TypographyBlockquote className={className}>
-          {children}
-        </TypographyBlockquote>
+        <TypographyBlockquote>{children}</TypographyBlockquote>
       ),
     },
 
