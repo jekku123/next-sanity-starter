fa6a640a8e6255d28f23aca18209c74a92e6cc31
Jesse Manninen <jesse.manninen@gmail.com>
2024-05-09 15:10:26 +0300

    fix type error at settingaction

--- src/lib/next-auth/actions/settings.ts
+++ src/lib/next-auth/actions/settings.ts
@@ -33,7 +33,7 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   if (values.email && values.email !== user.email) {
     const existingUser = await getUserByEmail(values.email);
 
-    if (existingUser && existingUser.id !== user.id) {
+    if (existingUser && existingUser._id !== user.id) {
       return { error: "Email already in use!" };
     }
 
