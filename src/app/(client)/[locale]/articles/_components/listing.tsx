9394a66fca55879e5704315f783f4f1d8d60a40a
Jesse Manninen <jesse.manninen@gmail.com>
2024-06-06 19:18:03 +0300

    articles listing page translations

--- src/app/(client)/[locale]/articles/_components/listing.tsx
+++ src/app/(client)/[locale]/articles/_components/listing.tsx
@@ -4,10 +4,12 @@ import { ArticleTeaser } from "@/components/documents/article-teasers";
 import { TypographyH1 } from "@/components/typography";
 import { Input } from "@/components/ui/input";
 import { ArticleTeaser as ArticleTeaserType } from "@/lib/zod/article-teaser";
+import { useTranslations } from "next-intl";
 import { useState } from "react";
 
 export default function Listing({ items }: { items: ArticleTeaserType[] }) {
   const [search, setSearch] = useState("");
+  const t = useTranslations("ArticlesListingPage");
 
   const filteredItems = items.filter((item) =>
     item.title.toLowerCase().includes(search.toLowerCase()),
@@ -17,12 +19,12 @@ export default function Listing({ items }: { items: ArticleTeaserType[] }) {
     <>
       <div className="flex items-center justify-between">
         <TypographyH1 className="text-center md:text-start">
-          Articles
+          {t("title")}
         </TypographyH1>
         <div>
           <Input
             onChange={(e) => setSearch(e.target.value)}
-            placeholder="Search.."
+            placeholder={t("search-placeholder")}
           />
         </div>
       </div>
