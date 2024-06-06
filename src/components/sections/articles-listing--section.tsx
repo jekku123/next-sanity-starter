3d2cb509fccba58b98ae67d4a1d224c1df60fc16
Jesse Manninen <jesse.manninen@gmail.com>
2024-06-06 17:45:30 +0300

    listing translations

--- src/components/sections/articles-listing--section.tsx
+++ src/components/sections/articles-listing--section.tsx
@@ -1,5 +1,6 @@
 import { getArticles } from "@/lib/sanity/utils/get-articles";
 import { ArticlesListing } from "@/lib/zod/section";
+import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 import ArticleTeasers from "../documents/article-teasers";
 import { TypographyH1 } from "../typography";
@@ -13,6 +14,7 @@ export default async function ArticlesListingSection({
   content: ArticlesListing;
 }) {
   const articles = await getArticles({ limit: content.limit || undefined });
+  const t = await getTranslations("ArticlesListingSection");
 
   return (
     <div className="flex flex-col gap-6">
@@ -21,12 +23,12 @@ export default async function ArticlesListingSection({
       {articles ? (
         <ArticleTeasers articles={articles} />
       ) : (
-        <p>No articles found</p>
+        <p>{t("no-articles")}</p>
       )}
 
       <div className="place-self-center">
         <Button asChild>
-          <Link href="/articles">View all articles</Link>
+          <Link href="/articles">{t("view-all-articles")}</Link>
         </Button>
       </div>
     </div>
